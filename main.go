41b6e312c127e5583ea3e3817f11dd4958b30f14
crizah <shaizah.labique@gmail.com>
2025-08-22 12:11:54 +0530

    started on the server

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 // import "shazam/shazam"
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	// "shazam/server"
+)
+
+// we need to be able to upgrade the connection from http to webSocket
 
 func main() {
-	fmt.Printf("compiles")
+
+	var handler http.Handler
+	http.Handle("/", handler)
+	log.Print("Starting server...")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 
 }
